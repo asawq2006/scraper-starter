b8279a2ced14300a497c482ec77c59aa851f4c63
Vivek Nair <vnair611@stanford.edu>
2014-09-27 10:17:50 -0700

    Add in the starter files.

---
+++ fetcher.js
@@ -0,0 +1,22 @@
+/* Fetch images from the given URLs into the provided directory.
+ *
+ * Arguments:
+ * directory -- the directory to put the images in
+ * urls -- an array of URLs to fetch images from
+ */
+exports.fetchImages = function(directory, urls) {
+  // TODO
+};
+
+/* Fetch an image given by the provided URL. Store the result in a file with
+ * name "[prefix].[extension]", where [prefix] is the provided prefix and
+ * [extension] is the correct extension of the fetched image.
+ *
+ * Arguments:
+ * url -- the URL to fetch the image from
+ * directory -- the directory to fetch the file into
+ * prefix -- the prefix of the file name
+ */
+function fetchSingleImage(url, directory, prefix) {
+  // TODO
+}
---
+++ scraper.js
@@ -0,0 +1,23 @@
+/* Scrapes the given URL for image links specified by the provided selector.
+ * Calls the callback with a list of URLs.
+ *
+ * Arguments:
+ * url -- the URL to scrape
+ * selector -- the CSS selector that identifies the image links
+ * callback -- the callback to call with the list of URLs
+ */
+exports.scrapeImageURLs = function(url, selector, callback) {
+  // TODO
+};
+
+
+/* Finds image links in the given HTML that match the provided selector.
+ * Returns a list of links.
+ *
+ * Arguments:
+ * html -- the HTML to parse
+ * selector -- the CSS selector that identifies the image links
+ */
+function findImageURLs(html, selector) {
+  // TODO
+}
